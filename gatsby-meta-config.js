17f90a568f4f6d62fa5093ce6fd73d95b2c30ba1
Junha <junha.ahn.dev@gmail.com>
2023-03-26 03:37:51 +0000

    update: utterances

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -16,7 +16,7 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'junha-ahn/devkly', // Your repository for archive comment
+    utterances: 'junha-ahn/dev-blog', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
